a1f90049ffaddbeabb4aa69f1c71090645065160
David Toth <dt1510@ic.ac.uk>
2013-06-04 21:17:03 -0700

    displaying the oldest word to learn.

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<h2>Learn English Vocabulary</h2>
 <?php
-function get_most_recent_word() {
+//Learn English Vocabulary
+function get_least_recent_word() {
 //    `ls data --sort time -1 | head -1`
     $path = "./data"; 
-
-    $latest_ctime = 0;
-    $latest_filename = '';    
-
+    $latest_ctime = -1;
+    $latest_filename = '';
     $d = dir($path);
     while (false !== ($entry = $d->read())) {
       $filepath = "{$path}/{$entry}";
       // could do also other checks than just checking whether the entry is a file
-      if (is_file($filepath) && filectime($filepath) > $latest_ctime) {
+      if (is_file($filepath) && ($latest_ctime == -1 || filectime($filepath) < $latest_ctime)) {
         $latest_ctime = filectime($filepath);
         $latest_filename = $entry;
       }
@@ -21,9 +19,11 @@ function get_most_recent_word() {
 }
     
     
-$word = get_most_recent_word();
-$recent_file = "./data/".$word.".txt";
-$sentences = file($recent_file, FILE_IGNORE_NEW_LINES);
+$word = get_least_recent_word();
+echo "<h2>$word</h2>";
+$old_file = "./data/".$word.".txt";
+$sentences = file($old_file, FILE_IGNORE_NEW_LINES);
+touch($old_file);
 foreach($sentences as $sentence) {
     $sentence = str_replace("$word", "<b>$word</b>", $sentence);
     echo "$sentence<br />";
